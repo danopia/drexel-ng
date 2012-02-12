d3162ca843b88cb114999e5524d362e63384d38b
unknown <danopia@danowin7.(none)>
2012-02-12 05:34:21 +0000

    Linked the realtime settings with the persistant settings.

--- BBSeven/Settings/SettingsWindow.xaml.vb
+++ BBSeven/Settings/SettingsWindow.xaml.vb
@@ -1,16 +1,27 @@
 ﻿Public Class SettingsWindow
 
+    Protected Friend Sub ApplySettings()
+        My.Settings.FontSize = CType(My.Application.Resources.Item("settings"), MySettings).FontSize
+    End Sub
+
+    Protected Friend Sub RevertSettings()
+        CType(My.Application.Resources.Item("settings"), MySettings).FontSize = My.Settings.FontSize
+    End Sub
+
     Private Sub ApplyButton_Click(sender As System.Object, e As System.Windows.RoutedEventArgs) Handles ApplyButton.Click
+        ApplySettings()
         My.Settings.Save()
     End Sub
 
     Private Sub OKButton_Click(sender As System.Object, e As System.Windows.RoutedEventArgs) Handles OKButton.Click
+        ApplySettings()
         My.Settings.Save()
         Me.Close()
     End Sub
 
     Private Sub CancelButton_Click(sender As System.Object, e As System.Windows.RoutedEventArgs) Handles CancelButton.Click
         My.Settings.Reload()
+        RevertSettings()
         Me.Close()
     End Sub
 
