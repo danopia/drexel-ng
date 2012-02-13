31ff43b91e51a91ce2a694527fe9f31c692992fa
unknown <danopia@danowin7.(none)>
2012-02-13 01:03:12 +0000

    Added a generic ListPane that works like the new MenuListPane

--- ShiftingPanes/Panes/ListPane.vb
+++ ShiftingPanes/Panes/ListPane.vb
@@ -23,3 +23,19 @@ Public Class ListPane(Of x, y As {New, ListItem(Of x)})
     End Sub
 
 End Class
+
+Public MustInherit Class ListPane(Of x)
+    Inherits ListPane(Of x, ListItem(Of x))
+
+    Public MustOverride Sub BuildItem(Source As x, Control As ListItem(Of x))
+
+    Public Shadows Sub SetSources(SourceList As List(Of x))
+        _stack.Children.Clear()
+        For Each Source As x In SourceList
+            Dim Control As New ListItem(Of x)
+            BuildItem(Source, Control)
+            _stack.Children.Add(Control)
+        Next
+    End Sub
+
+End Class
